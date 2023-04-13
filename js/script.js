fa195f0f04b62ac8e226c65b6c6bc0a35f54b610
David Calaiò <davidcjr11@gmail.com>
2023-04-13 23:29:42 +0200

    last commit

--- js/script.js
+++ js/script.js
@@ -9,6 +9,24 @@ data(){
         image,
         counter : 0,
     }
-}
+},
+
+methods:{
+    nextPre(next){
+        //faccio funziona lo slide aumentando e diminuendo il contatore
+        if(next)this.counter++;
+        else this.counter--;
+
+        //faccio il controllo per per lo slide
+        /*
+        if(this.counter === this.image.lenght){
+            this.counter = 0;  
+        }
+        if(this.counter < 0) this.counter = this.image.lenght -1 
+        
+        */
+        console.log(this.counter);
+        }
+    }
 
 }).mount('#app')
